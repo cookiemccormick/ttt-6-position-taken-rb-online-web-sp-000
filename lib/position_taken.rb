10aa9e881d89397be5233ffcae6696b4f22466a6
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-09-29 19:13:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,12 @@
 # code your #position_taken? method here!
+<<<<<<< HEAD
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else 
     true
   end
+=======
+def position_taken(board, index)
+>>>>>>> 22e412f39171548289f323cf36bdd2cdbfb5bd29
 end
